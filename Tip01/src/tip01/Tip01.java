63e7c27429137ea5a0ccbf53ba9ab5a897b1c3d8
Jamin Brako <jbrako@ucvts.tec.nj.us>
2020-03-11 12:48:11 -0400

    (finished) Tip01

--- Tip01/src/tip01/Tip01.java
+++ Tip01/src/tip01/Tip01.java
@@ -15,17 +15,27 @@ public class Tip01 {
         Person 7: $11
         Person 8: $30
         */
+        int person1 = 10;        
         double tax = 1.05;
         double tip = 1.15;
+        double total;
+        total = tax * tip;
         
-        int person1 = 10;
-        System.out.println((int)person1 * tax * tip );
-        int person2;
-        int person3;
-        int person4;
-        int person5;
-        int person6;
-        int person7;
-        int person8;
+        System.out.println("$" + Math.floor(person1 * total));
+        int person2 = 12;
+        
+        System.out.println("$" + person2 * total);
+        int person3 = 9;
+        System.out.println("$" + (double)(person3 * total));
+        int person4 = 8;
+        System.out.println("$" + (person4 * total));
+        int person5 = 7;
+        System.out.println("$" + person5 * total);
+        int person6 = 15;
+        System.out.println("$" + person6 * total);
+        int person7 = 11;
+        System.out.println("$" + person7 * total);
+        int person8 = 30;
+        System.out.println("$" + person8 * total);
     }    
 }
