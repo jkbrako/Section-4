fafdd0f015b8511e5698b7391fc33f465a50dadf
Jamin Brako <jamin.brako@gmail.com>
2020-03-26 23:09:25 -0400

    Finished Tip05

--- ImportEx/src/com/example/AddImport.java
+++ ImportEx/src/com/example/AddImport.java
@@ -5,13 +5,13 @@
  */
 package com.example;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
+import javax.swing.*;
 
 public class AddImport {
 
     public static void main(String args[]) {
-        javax.swing.JLabel label = new javax.swing.JLabel("hello");
+        JLabel label = new JLabel("hello");
 
     }
 
